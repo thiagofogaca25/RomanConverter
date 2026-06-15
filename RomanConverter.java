e69a817bfeb04aff2ff075b45c41a7756be3cf16
thiago <thiagofogaca25@gmail.com>
2026-06-14 22:05:00 -0300

    add modules

--- RomanConverter.java
+++ RomanConverter.java
@@ -14,24 +14,7 @@ public class RomanConverter {
                 continue;
             }
 
-            int result = 0;
-            for (int i = 0; i < roman.length(); i++) {
-                char ActualRomanChar = roman.charAt(i);
-                int actualRomanValue = romanToValue(ActualRomanChar);
-                if (i < roman.length() - 1) {
-                    char NextRomanChar = roman.charAt(i + 1);
-                    int nextRomanValue = romanToValue(NextRomanChar);
-                    if (actualRomanValue < nextRomanValue) {
-                        result -= actualRomanValue;
-                    }
-                    if (actualRomanValue >= nextRomanValue) {
-                        result += actualRomanValue;
-                    }
-                } else {
-                    result += actualRomanValue;
-                }
-
-            }
+            int result = romanToInt(roman);
 
             System.out.println(result);
             System.out.println("Proximo?(y/n)");
@@ -43,6 +26,28 @@ public class RomanConverter {
         System.out.println("GoodBye");
 
         scanner.close();
+    }
+
+    public static int romanToInt(String roman) {
+        int result = 0;
+        for (int i = 0; i < roman.length(); i++) {
+            char ActualRomanChar = roman.charAt(i);
+            int actualRomanValue = romanToValue(ActualRomanChar);
+            if (i < roman.length() - 1) {
+                char NextRomanChar = roman.charAt(i + 1);
+                int nextRomanValue = romanToValue(NextRomanChar);
+                if (actualRomanValue < nextRomanValue) {
+                    result -= actualRomanValue;
+                }
+                if (actualRomanValue >= nextRomanValue) {
+                    result += actualRomanValue;
+                }
+            } else {
+                result += actualRomanValue;
+            }
+
+        }
+        return result;
     }
 
     public static int romanToValue(char roman) {
@@ -71,11 +76,19 @@ public class RomanConverter {
     }
 
     public static boolean correct(String roman) {
+        return isValidLength(roman) && isValidRepetition(roman) && isValidRomanLength(roman)
+                && isValidSubtraction(roman);
+    }
 
+    public static boolean isValidLength(String roman) {
         if (roman.length() >= 15) {
             System.out.println("Invalid Roman numeral");
             return false;
         }
+        return true;
+    }
+
+    public static boolean isValidRomanLength(String roman) {
         for (int i = 0; i < roman.length(); i++) {
             char c = roman.charAt(i);
 
@@ -92,34 +105,15 @@ public class RomanConverter {
 
             }
         }
+        return true;
+    }
 
-        int counterRepetition = 1;
+    public static boolean isValidSubtraction(String roman) {
         for (int i = 0; i < roman.length(); i++) {
             char currentChar = roman.charAt(i);
 
             if (i < roman.length() - 1) {
                 char nextChar = roman.charAt(i + 1);
-
-                if (currentChar == nextChar) {
-                    counterRepetition++;
-                } else {
-                    counterRepetition = 1;
-                }
-
-                if ((currentChar == 'V' ||
-                        currentChar == 'L' ||
-                        currentChar == 'D') &&
-                        currentChar == nextChar) {
-
-                    System.out.println("Invalid Roman numeral");
-                    return false;
-                }
-
-                if (counterRepetition > 3) {
-                    System.out.println("Invalid Roman numeral");
-                    return false;
-                }
-
                 int actualRomanValue = romanToValue(currentChar);
                 int nextRomanValue = romanToValue(nextChar);
 
@@ -150,9 +144,43 @@ public class RomanConverter {
                     }
                 }
             }
-
         }
 
+        return true;
+
+    }
+
+    public static boolean isValidRepetition(String roman) {
+        int counterRepetition = 1;
+        for (int i = 0; i < roman.length(); i++) {
+            char currentChar = roman.charAt(i);
+
+            if (i < roman.length() - 1) {
+                char nextChar = roman.charAt(i + 1);
+
+                if (currentChar == nextChar) {
+                    counterRepetition++;
+                } else {
+                    counterRepetition = 1;
+                }
+
+                if ((currentChar == 'V' ||
+                        currentChar == 'L' ||
+                        currentChar == 'D') &&
+                        currentChar == nextChar) {
+
+                    System.out.println("Invalid Roman numeral");
+                    return false;
+                }
+
+                if (counterRepetition > 3) {
+                    System.out.println("Invalid Roman numeral");
+                    return false;
+                }
+
+            }
+
+        }
         return true;
     }
 }
